d4748441cbe2e792fc83bdca46fdf895af75927a
Rishav20-22 <52622186+Rishav20-22@users.noreply.github.com>
2022-04-08 15:04:37 -0700

    Add files via upload

--- Hmwk6.java
+++ Hmwk6.java
@@ -1,94 +1,96 @@
 /*=============================================================================
- |   Assignment:  HW6
- |       Author:  Rishav Kumar - rishavk@email.arizona.edu
+ |   Assignment:  Program #6:  Homework #6
+ |       Author:  Josiah Lane (josiahlane01@email.arizona.edu)
+ |       Grader:  Ash Reed?
  |
- |       Course:  CSC 245, Spring 22
+ |       Course:  CSc 245
  |   Instructor:  L. McCann
- | Sect. Leader:  NA
- |     Due Date:  04/08/22 3:00 pm
+ |     Due Date:  4/8/22 3:05pm
  |
- |     Language:  Java
- |     Packages:  None
- |  Compile/Run:  java hmwk6.java [integer value]
+ |  Description:  This program demonstrates if a given integer is either prime or can be shown to be expressible as the product of two or more primes.
  |
- +-----------------------------------------------------------------------------
- |
- |  Description: Prove Fundamental theorem of Arithmetic
+ |     Language:  Java 15
+ | Ex. Packages:  N/A
  |                
- |        Input:  Pass an integer in command line
- |
- |       Output:  Shows the individual prime factors to prove the FToA
- |
- |   Techniques:  Using recursion
- |
- |
- |   Known Bugs:  I haven't got any errors while testing.
- |
+ | Deficiencies:  I know of no illogical fallacies. The code does not implement proper spacing, but the logic should hold true for FToA.
  *===========================================================================*/
-public class hmwk6 {
-    public static int factors(int x){
-    /*
-    Returns the largest factor for the passed integer
-    parameters: integer 
-    */
-        for(int i=x-1;i>1;i--){
-            if(x%i == 0){
-                return i;
-            }
-        }
-        return 1;
-    }
-    static boolean isPrime(int n)
-    {
-        /*
-        Checks if a given number is prime.
-        */
+public class Hmwk6 {
+	int intTest;
+	
+	public static void main(String[] args) {
+		
+		int num = Integer.valueOf(36);
+		System.out.println("$ java Hmwk6 " + num);
+		System.out.println("This program will demonstrate that " + num + " is either prime or is the product of two or more prime numbers");
+		System.out.println();
+		
+		if(!(num >= 2)) {
+			System.out.println(num + " is not a positive integer >= 2 thus is not prime or is the product of two or more prime numbers.");
+		} else {
+			prime(num, 0);
+		}
+		
+		System.out.println("As this output shows, the Fundamental Theorem of Arithmetic holds for " + num);
 
-        if (n == 1)
-            return false;
-        for (int i = 2; i < n; i++)
-            if (n % i == 0)
-                return false;
-  
-        return true;
-    }
-    public static void main(String args[]){
-        int x = Integer.parseInt((args[0]));
-        int flagX = x;
-        int indent = 0;
-        System.out.println("This program will demonstrate that "+x+" is either prime\nor is the product of two or more prime numbers.");
-        System.out.println();
-        System.out.println();
-        while(true){
-            for(int i=0;i<indent;i++)
-            System.out.print("    ");
-            if(x/factors(x) == factors(x)){
-                System.out.println(x+" = "+factors(x)+" squared; are these factors either prime or products of primes?");
-            }
-            else
-            {
-            System.out.println(x+" = "+factors(x)+" * "+x/factors(x)+"; are these factors either prime or products of primes?");
-            }
-            if(isPrime(x/factors(x))){
-                for(int i=0;i<indent;i++)
-                System.out.print("    ");
-                System.out.println(x/factors(x)+" is Prime");
-            }
-            
-            if(isPrime(factors(x))){
-                System.out.println(x+" is the product of primes ("+x/factors(x)+" and "+factors(x)+" are prime or prime Products");
-                for(int i=0;i<indent;i++)
-                System.out.print("    ");
-                System.out.println(factors(x)+" is Prime");
-                break;
-            }
-            else
-            {
-                x=factors(x);
-                indent++;
-            }
-        }
-        System.out.println("As this output shows, the Fundamental Theorem of Arithmetic holds for "+flagX+".");
- 
-    }
+	}
+
+	  /*---------------------------------------------------------------------
+    |  Method prime
+    |
+    |  Purpose:  This method checks if a number is prime
+    |
+    |  Pre-condition: positive integer >= 2
+    |
+    |  Post-condition: Integer is prime
+    |
+    |  Parameters:
+    |      num -- integer number that we're testing for primes.
+    |      iter -- integer number of iterations.
+    |
+    |  Returns:  
+    *-------------------------------------------------------------------*/
+
+	private static void prime(int num, int iter) {
+		
+		if (num%2 == 0) {
+			int leftVal = num/2;
+			System.out.println("    ".repeat(iter) + num + " = " + num/2 + " * " + "2; are these factors either prime or products of primes?");
+			prime(leftVal, iter++);
+			//System.out.println("2 is prime.");
+			
+		} else if (num%3==0) {
+			int leftVal = num/3;
+			if (num/3 == 3) {
+				System.out.println(num + " = 3 squared");
+			}else {
+				System.out.println("    ".repeat(iter) + num + " = " + num/3 + " * " + "3; are these factors either prime or products of primes?");
+			}
+			
+			prime(leftVal, iter++);
+			//System.out.println("3 is prime.");
+			
+		} else if (num%5==0) {
+			int leftVal = num/5;
+			System.out.println("    ".repeat(iter) + num + " = " + num/2 + " * " + "5; are these factors either prime or products of primes?");
+			prime(leftVal, iter++);
+			//System.out.println("5 is prime.");
+			
+		} else if (num%7==0) {
+			int leftVal = num/7;
+			System.out.println("    ".repeat(iter) + num + " = " + num/2 + " * " + "7; are these factors either prime or products of primes?");
+			prime(leftVal, iter++);
+			//System.out.println("7 is prime/.");
+			
+		} else if (num%11==0) {
+			int leftVal = num/11;
+			System.out.println("    ".repeat(iter) + num + " = " + num/2 + " * " + "11; are these factors either prime or products of primes?");
+			prime(leftVal,iter++);
+			//System.out.println("11 is prime.");
+		
+		}
+		System.out.println(num + " is prime.");
+		
+		
+		
+	}
 }
